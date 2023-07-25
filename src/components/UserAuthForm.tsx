f4d2504011fc7910e77e26ff3ecbd75a48fdeed7
Abhay Bhardwaj <abhaybhardwaj@Abhays-MacBook-Air.local>
2023-07-25 05:32:04 +0530

    initial project commit

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { toast } = useToast();
+
+	const loginWithgoogle = async () => {
+		setIsLoading(true);
+
+		try {
+			await signIn("google");
+		} catch (error) {
+			// toast notifiaction
+			toast({
+				title: "There was a problem.",
+				description: "There was an error logging with Goggle",
+				variant: "destructive"
+			})
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<div className={cn("flex justify-center", className)} {...props}>
+			<Button onClick={loginWithgoogle} isLoading={isLoading} size="sm" className="w-full">
+				{isLoading? null : <Icons.google className="h-4 w-4 mr-2" />}
+				Google
+			</Button>
+		</div>
+	);
+};
+
+export default UserAuthForm;
